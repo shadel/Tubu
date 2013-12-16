c2b273ea64ee1d374a82ce06f43f02074de75103
Vo Ngoc Trieu <vdo01-vntrieu@asp-internet.co.jp>
2013-12-16 17:38:33 +0700

    Add design

---
+++ index.php
@@ -0,0 +1,579 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+<title>Radio</title>
+<link
+	href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300italic,400italic,700italic,400,300,700&subset=latin,vietnamese'
+	rel='stylesheet' type='text/css'>
+
+
+<link href="style.css" rel="stylesheet" type="text/css" />
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
+<script src="https://code.jquery.com/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Css -->
+<script>
+ $(window).bind('scroll', function() {
+         if ($(window).scrollTop() > 50) {
+             $('#category-top-1').hide();
+			 $('#category-top-2').show();
+         }
+         else {
+             $('#category-top-1').show();
+			 $('#category-top-2').hide();
+         }
+    });
+	</script>
+
+
+</head>
+<body>
+	<div class="top-bar menu-mb">
+		<div class="top-ct container clearfix">
+			<div class="logo left"></div>
+			<div class="menu left hidden-xs">
+				<ul class="clearfix ">
+					<li class="left dropdown " id="category-top-2" style="display:none; color: #fff">
+						<a href="#" style="color: #fff" class="dropdown-toggle" data-toggle="dropdown">Dã sử & Kiếm hiệp <b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Tất cả</a></li>
+							<li><a href="#">Truyện ngắn</a></li>
+							<li><a href="#">Truyện dài</a></li>
+							<li><a href="#">Truyện cười</a></li>
+							<li><a href="#">Dã sử & Kiếm hiệp</a></li>
+							<li><a href="#">Ý nghĩa cuộc sống</a></li>
+							<li><a href="#">Tình yêu</a></li>
+							<li><a href="#">Kinh dị</a></li>
+						</ul>
+					</li>
+					<li class="left">Truyện</li>
+					<li class="left">Audio</li>
+				</ul>
+			</div>
+			<div class="menu visible-xs">
+				MENU
+			</div>
+			<div></div>
+			<div class="login right hide clearfix">
+				<div class="btn facebook-btn left">Đăng Nhập</div>
+				<div class="btn google-btn left">Đăng ký</div>
+
+			</div>
+		</div>
+	</div>
+	<div class="header" style="margin-top:40px;">
+		<div class="container">
+		<!-- Split button -->
+			<div class="btn-group" id="category-top-1" style="margin-top: 30px;">
+			  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">Dã sử & Kiếm hiệp <span class="caret"></span></button>
+			  <ul class="dropdown-menu">
+				<li><a href="#">Tất cả</a></li>
+				<li><a href="#">Truyện ngắn</a></li>
+				<li><a href="#">Truyện dài</a></li>
+				<li><a href="#">Truyện cười</a></li>
+				<li><a href="#">Dã sử & Kiếm hiệp</a></li>
+				<li><a href="#">Ý nghĩa cuộc sống</a></li>
+				<li><a href="#">Tình yêu</a></li>
+				<li><a href="#">Kinh dị</a></li>
+			  </ul>
+			</div>
+		
+		</div>
+	
+	</div>
+	<div class="nav hide"></div>
+	<div class="container">
+		
+
+		<div class="row">
+			<div class="story-item col-md-3 col-sm-4 col-xs-12">
+				<div class="hidden-xs">
+					<div class="content">
+						<div class="pic">
+							<a class="thumbnail" href="/book/204744/index.html"><img trung
+								alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师"
+								src="http://fm2.xs8xs8.cn/data/cover/84/204744.jpg"> </a>
+						</div>
+						<h2>
+							<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+						</h2>
+						<p>都市异能爽文，女强男更强，双强联手，结局一对一！ …… 有女初生，手纹有异，手眼通天，可掌阴阳！
+							当她重生在胆小怯弱的她身上，势要活出精彩时，被一句天煞孤星断定一生！ 而一张小小的纸衣竟能将她的姻缘买断！
+							可谁来告诉她，她那所谓的未婚夫却是…</p>
+						<dl class="xq">
+							<dd>
+								作者：<span>兮菲</span><br> 更新：<span>6.3万</span><br> 字数：<span>19.1万</span>
+							</dd>
+						</dl>
+					</div>
+				</div>
+				<div class="visible-xs">
+					<div class="content">
+						<div class="media pic">
+								<a class="pull-left thumbnail img-circle" href="/book/204744/index.html"> <img
+								data-src="http://getbootstrap.com/docs-assets/js/holder.js/50x50"
+									alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师" class="media-object"
+									src="http://fm2.xs8xs8.cn/data/cover/84/204744.jpg">
+								</a>
+							<div class="media-body">
+								<h5 class="media-heading">
+									<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+								</h5>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="story-item col-md-3 col-sm-4 col-xs-12">
+				<div class="hidden-xs">
+					<div class="content">
+						<div class="pic">
+							<a class="thumbnail" href="/book/204744/index.html"><img trung
+								alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师"
+								src="http://fm2.xs8xs8.cn/data/cover/5c/175605.jpg"> </a>
+						</div>
+						<h2>
+							<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+						</h2>
+						<p>都市异能爽文，女强男更强，双强联手，结局一对一！ …… 有女初生，手纹有异，手眼通天，可掌阴阳！
+							当她重生在胆小怯弱的她身上，势要活出精彩时，被一句天煞孤星断定一生！ 而一张小小的纸衣竟能将她的姻缘买断！
+							可谁来告诉她，她那所谓的未婚夫却是…</p>
+						<dl class="xq">
+							<dd>
+								作者：<span>兮菲</span><br> 更新：<span>6.3万</span><br> 字数：<span>19.1万</span>
+							</dd>
+						</dl>
+					</div>
+				</div>
+				<div class="visible-xs">
+					<div class="content">
+						<div class="media pic">
+								<a class="pull-left thumbnail" href="/book/204744/index.html"> <img
+								data-src="http://getbootstrap.com/docs-assets/js/holder.js/50x50"
+									alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师" class="media-object"
+									src="http://fm2.xs8xs8.cn/data/cover/5c/175605.jpg">
+								</a>
+							<div class="media-body">
+								<h5 class="media-heading">
+									<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+								</h5>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="story-item col-md-3 col-sm-4 col-xs-12">
+				<div class="hidden-xs">
+					<div class="content">
+						<div class="pic">
+							<a class="thumbnail" href="/book/204744/index.html"><img trung
+								alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师"
+								src="http://fm4.xs8xs8.cn/data/cover/28/173972.jpg"> </a>
+						</div>
+						<h2>
+							<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+						</h2>
+						<p>都市异能爽文，女强男更强，双强联手，结局一对一！ …… 有女初生，手纹有异，手眼通天，可掌阴阳！
+							当她重生在胆小怯弱的她身上，势要活出精彩时，被一句天煞孤星断定一生！ 而一张小小的纸衣竟能将她的姻缘买断！
+							可谁来告诉她，她那所谓的未婚夫却是…</p>
+						<dl class="xq">
+							<dd>
+								作者：<span>兮菲</span><br> 更新：<span>6.3万</span><br> 字数：<span>19.1万</span>
+							</dd>
+						</dl>
+					</div>
+				</div>
+				<div class="visible-xs">
+					<div class="content">
+						<div class="media pic">
+								<a class="pull-left thumbnail" href="/book/204744/index.html"> <img
+								data-src="http://getbootstrap.com/docs-assets/js/holder.js/50x50"
+									alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师" class="media-object"
+									src="http://fm4.xs8xs8.cn/data/cover/28/173972.jpg">
+								</a>
+							<div class="media-body">
+								<h5 class="media-heading">
+									<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+								</h5>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="story-item col-md-3 col-sm-4 col-xs-12">
+				<div class="hidden-xs">
+					<div class="content">
+						<div class="pic">
+							<a class="thumbnail" href="/book/204744/index.html"><img trung
+								alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师"
+								src="http://fm4.xs8xs8.cn/data/cover/23/202292.jpg"> </a>
+						</div>
+						<h2>
+							<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+						</h2>
+						<p>都市异能爽文，女强男更强，双强联手，结局一对一！ …… 有女初生，手纹有异，手眼通天，可掌阴阳！
+							当她重生在胆小怯弱的她身上，势要活出精彩时，被一句天煞孤星断定一生！ 而一张小小的纸衣竟能将她的姻缘买断！
+							可谁来告诉她，她那所谓的未婚夫却是…</p>
+						<dl class="xq">
+							<dd>
+								作者：<span>兮菲</span><br> 更新：<span>6.3万</span><br> 字数：<span>19.1万</span>
+							</dd>
+						</dl>
+					</div>
+				</div>
+				<div class="visible-xs">
+					<div class="content">
+						<div class="media pic">
+								<a class="pull-left thumbnail" href="/book/204744/index.html"> <img
+								data-src="http://getbootstrap.com/docs-assets/js/holder.js/50x50"
+									alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师" class="media-object"
+									src="http://fm4.xs8xs8.cn/data/cover/23/202292.jpg">
+								</a>
+							<div class="media-body">
+								<h5 class="media-heading">
+									<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+								</h5>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="story-item col-md-3 col-sm-4 col-xs-12">
+				<div class="hidden-xs">
+					<div class="content">
+						<div class="pic">
+							<a class="thumbnail" href="/book/204744/index.html"><img trung
+								alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师"
+								src="http://fm2.xs8xs8.cn/data/cover/f0/207457.jpg"> </a>
+						</div>
+						<h2>
+							<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+						</h2>
+						<p>都市异能爽文，女强男更强，双强联手，结局一对一！ …… 有女初生，手纹有异，手眼通天，可掌阴阳！
+							当她重生在胆小怯弱的她身上，势要活出精彩时，被一句天煞孤星断定一生！ 而一张小小的纸衣竟能将她的姻缘买断！
+							可谁来告诉她，她那所谓的未婚夫却是…</p>
+						<dl class="xq">
+							<dd>
+								作者：<span>兮菲</span><br> 更新：<span>6.3万</span><br> 字数：<span>19.1万</span>
+							</dd>
+						</dl>
+					</div>
+				</div>
+				<div class="visible-xs">
+					<div class="content">
+						<div class="media pic">
+								<a class="pull-left thumbnail" href="/book/204744/index.html"> <img
+								data-src="http://getbootstrap.com/docs-assets/js/holder.js/50x50"
+									alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师" class="media-object"
+									src="http://fm2.xs8xs8.cn/data/cover/f0/207457.jpg">
+								</a>
+							<div class="media-body">
+								<h5 class="media-heading">
+									<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+								</h5>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="story-item col-md-3 col-sm-4 col-xs-12">
+				<div class="hidden-xs">
+					<div class="content">
+						<div class="pic">
+							<a class="thumbnail" href="/book/204744/index.html"><img trung
+								alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师"
+								src="http://fm2.xs8xs8.cn/data/cover/6c/198889.jpg"> </a>
+						</div>
+						<h2>
+							<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+						</h2>
+						<p>都市异能爽文，女强男更强，双强联手，结局一对一！ …… 有女初生，手纹有异，手眼通天，可掌阴阳！
+							当她重生在胆小怯弱的她身上，势要活出精彩时，被一句天煞孤星断定一生！ 而一张小小的纸衣竟能将她的姻缘买断！
+							可谁来告诉她，她那所谓的未婚夫却是…</p>
+						<dl class="xq">
+							<dd>
+								作者：<span>兮菲</span><br> 更新：<span>6.3万</span><br> 字数：<span>19.1万</span>
+							</dd>
+						</dl>
+					</div>
+				</div>
+				<div class="visible-xs">
+					<div class="content">
+						<div class="media pic">
+								<a class="pull-left thumbnail" href="/book/204744/index.html"> <img
+								data-src="http://getbootstrap.com/docs-assets/js/holder.js/50x50"
+									alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师" class="media-object"
+									src="http://fm2.xs8xs8.cn/data/cover/84/204744.jpg">
+								</a>
+							<div class="media-body">
+								<h5 class="media-heading">
+									<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+								</h5>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="story-item col-md-3 col-sm-4 col-xs-12">
+				<div class="hidden-xs">
+					<div class="content">
+						<div class="pic">
+							<a class="thumbnail" href="/book/204744/index.html"><img trung
+								alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师"
+								src="http://fm4.xs8xs8.cn/data/cover/20/184416.jpg"> </a>
+						</div>
+						<h2>
+							<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+						</h2>
+						<p>都市异能爽文，女强男更强，双强联手，结局一对一！ …… 有女初生，手纹有异，手眼通天，可掌阴阳！
+							当她重生在胆小怯弱的她身上，势要活出精彩时，被一句天煞孤星断定一生！ 而一张小小的纸衣竟能将她的姻缘买断！
+							可谁来告诉她，她那所谓的未婚夫却是…</p>
+						<dl class="xq">
+							<dd>
+								作者：<span>兮菲</span><br> 更新：<span>6.3万</span><br> 字数：<span>19.1万</span>
+							</dd>
+						</dl>
+					</div>
+				</div>
+				<div class="visible-xs">
+					<div class="content">
+						<div class="media pic">
+								<a class="pull-left thumbnail" href="/book/204744/index.html"> <img
+								data-src="http://getbootstrap.com/docs-assets/js/holder.js/50x50"
+									alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师" class="media-object"
+									src="http://fm2.xs8xs8.cn/data/cover/84/204744.jpg">
+								</a>
+							<div class="media-body">
+								<h5 class="media-heading">
+									<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+								</h5>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="story-item col-md-3 col-sm-4 col-xs-12">
+				<div class="hidden-xs">
+					<div class="content">
+						<div class="pic">
+							<a class="thumbnail" href="/book/204744/index.html"><img trung
+								alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师"
+								src="http://fm2.xs8xs8.cn/data/cover/a3/183360.jpg"> </a>
+						</div>
+						<h2>
+							<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+						</h2>
+						<p>都市异能爽文，女强男更强，双强联手，结局一对一！ …… 有女初生，手纹有异，手眼通天，可掌阴阳！
+							当她重生在胆小怯弱的她身上，势要活出精彩时，被一句天煞孤星断定一生！ 而一张小小的纸衣竟能将她的姻缘买断！
+							可谁来告诉她，她那所谓的未婚夫却是…</p>
+						<dl class="xq">
+							<dd>
+								作者：<span>兮菲</span><br> 更新：<span>6.3万</span><br> 字数：<span>19.1万</span>
+							</dd>
+						</dl>
+					</div>
+				</div>
+				<div class="visible-xs">
+					<div class="content">
+						<div class="media pic">
+								<a class="pull-left thumbnail" href="/book/204744/index.html"> <img
+								data-src="http://getbootstrap.com/docs-assets/js/holder.js/50x50"
+									alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师" class="media-object"
+									src="http://fm2.xs8xs8.cn/data/cover/84/204744.jpg">
+								</a>
+							<div class="media-body">
+								<h5 class="media-heading">
+									<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+								</h5>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="story-item col-md-3 col-sm-4 col-xs-12">
+				<div class="hidden-xs">
+					<div class="content">
+						<div class="pic">
+							<a class="thumbnail" href="/book/204744/index.html"><img trung
+								alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师"
+								src="http://fm2.xs8xs8.cn/data/cover/11/187076.jpg"> </a>
+						</div>
+						<h2>
+							<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+						</h2>
+						<p>都市异能爽文，女强男更强，双强联手，结局一对一！ …… 有女初生，手纹有异，手眼通天，可掌阴阳！
+							当她重生在胆小怯弱的她身上，势要活出精彩时，被一句天煞孤星断定一生！ 而一张小小的纸衣竟能将她的姻缘买断！
+							可谁来告诉她，她那所谓的未婚夫却是…</p>
+						<dl class="xq">
+							<dd>
+								作者：<span>兮菲</span><br> 更新：<span>6.3万</span><br> 字数：<span>19.1万</span>
+							</dd>
+						</dl>
+					</div>
+				</div>
+				<div class="visible-xs">
+					<div class="content">
+						<div class="media pic">
+								<a class="pull-left thumbnail" href="/book/204744/index.html"> <img
+								data-src="http://getbootstrap.com/docs-assets/js/holder.js/50x50"
+									alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师" class="media-object"
+									src="http://fm2.xs8xs8.cn/data/cover/84/204744.jpg">
+								</a>
+							<div class="media-body">
+								<h5 class="media-heading">
+									<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+								</h5>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="story-item col-md-3 col-sm-4 col-xs-12">
+				<div class="hidden-xs">
+					<div class="content">
+						<div class="pic">
+							<a class="thumbnail" href="/book/204744/index.html"><img trung
+								alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师"
+								src="http://fm3.xs8xs8.cn/data/cover/08/202335.jpg"> </a>
+						</div>
+						<h2>
+							<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+						</h2>
+						<p>都市异能爽文，女强男更强，双强联手，结局一对一！ …… 有女初生，手纹有异，手眼通天，可掌阴阳！
+							当她重生在胆小怯弱的她身上，势要活出精彩时，被一句天煞孤星断定一生！ 而一张小小的纸衣竟能将她的姻缘买断！
+							可谁来告诉她，她那所谓的未婚夫却是…</p>
+						<dl class="xq">
+							<dd>
+								作者：<span>兮菲</span><br> 更新：<span>6.3万</span><br> 字数：<span>19.1万</span>
+							</dd>
+						</dl>
+					</div>
+				</div>
+				<div class="visible-xs">
+					<div class="content">
+						<div class="media pic">
+								<a class="pull-left thumbnail" href="/book/204744/index.html"> <img
+								data-src="http://getbootstrap.com/docs-assets/js/holder.js/50x50"
+									alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师" class="media-object"
+									src="http://fm2.xs8xs8.cn/data/cover/84/204744.jpg">
+								</a>
+							<div class="media-body">
+								<h5 class="media-heading">
+									<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+								</h5>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="story-item col-md-3 col-sm-4 col-xs-12">
+				<div class="hidden-xs">
+					<div class="content">
+						<div class="pic">
+							<a class="thumbnail" href="/book/204744/index.html"><img trung
+								alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师"
+								src="http://fm4.xs8xs8.cn/data/cover/cc/151856.jpg"> </a>
+						</div>
+						<h2>
+							<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+						</h2>
+						<p>都市异能爽文，女强男更强，双强联手，结局一对一！ …… 有女初生，手纹有异，手眼通天，可掌阴阳！
+							当她重生在胆小怯弱的她身上，势要活出精彩时，被一句天煞孤星断定一生！ 而一张小小的纸衣竟能将她的姻缘买断！
+							可谁来告诉她，她那所谓的未婚夫却是…</p>
+						<dl class="xq">
+							<dd>
+								作者：<span>兮菲</span><br> 更新：<span>6.3万</span><br> 字数：<span>19.1万</span>
+							</dd>
+						</dl>
+					</div>
+				</div>
+				<div class="visible-xs">
+					<div class="content">
+						<div class="media pic">
+								<a class="pull-left thumbnail" href="/book/204744/index.html"> <img
+								data-src="http://getbootstrap.com/docs-assets/js/holder.js/50x50"
+									alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师" class="media-object"
+									src="http://fm2.xs8xs8.cn/data/cover/84/204744.jpg">
+								</a>
+							<div class="media-body">
+								<h5 class="media-heading">
+									<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+								</h5>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		
+		<div class="story-item col-md-3 col-sm-4 col-xs-12">
+				<div class="hidden-xs">
+					<div class="content">
+						<div class="pic">
+							<a class="thumbnail" href="/book/204744/index.html"><img trung
+								alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师"
+								src="http://fm1.xs8xs8.cn/data/cover/de/204253.jpg"> </a>
+						</div>
+						<h2>
+							<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+						</h2>
+						<p>都市异能爽文，女强男更强，双强联手，结局一对一！ …… 有女初生，手纹有异，手眼通天，可掌阴阳！
+							当她重生在胆小怯弱的她身上，势要活出精彩时，被一句天煞孤星断定一生！ 而一张小小的纸衣竟能将她的姻缘买断！
+							可谁来告诉她，她那所谓的未婚夫却是…</p>
+						<dl class="xq">
+							<dd>
+								作者：<span>兮菲</span><br> 更新：<span>6.3万</span><br> 字数：<span>19.1万</span>
+							</dd>
+						</dl>
+					</div>
+				</div>
+				<div class="visible-xs">
+					<div class="content">
+						<div class="media pic">
+								<a class="pull-left thumbnail col-xs-2" href="/book/204744/index.html"> <img
+									alt="阴阳眼，鬼才通灵师" title="阴阳眼，鬼才通灵师" class="media-object"
+									src="http://fm1.xs8xs8.cn/data/cover/de/204253.jpg">
+								</a>
+							<div class="media-body  col-xs-10">
+								<h4 class="media-heading">
+									<a href="/book/204744/index.html">阴阳眼，鬼才通灵师</a>
+								</h4>
+								<p>都市异能爽文，女强男更强，双强联手，结局一对一！ …… 有女初生，手纹有异，手眼通天，可掌阴阳！
+									当她重生在胆小怯弱的她身上，势要活出精彩时，被一句天煞孤星断定一生！ 而一张小小的纸衣竟能将她的姻缘买断！
+									可谁来告诉她，她那所谓的未婚夫却是…</p>
+								<dl class="xq">
+									<dd>
+										作者：<span>兮菲</span><br> 更新：<span>6.3万</span><br> 字数：<span>19.1万</span>
+									</dd>
+								</dl>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			
+
+		</div>
+
+	</div>
+	<div class="footer"></div>
+</body>
+</html>
